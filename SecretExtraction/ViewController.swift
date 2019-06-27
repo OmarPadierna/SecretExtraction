3ec5a644c39c47b7cfb6a573092ff93eac10cc29
Omar Padierna <opadierna@guestlogix.com>
2019-06-27 11:59:28 -0400

    Initial Commit

---
+++ SecretExtraction/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SecretExtraction
+//
+//  Created by Omar Padierna on 2019-06-27.
+//  Copyright © 2019 Omar Padierna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
